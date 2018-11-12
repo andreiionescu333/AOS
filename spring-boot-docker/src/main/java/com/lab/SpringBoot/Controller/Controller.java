5cf893e39f73ed2a763ceed7a8696cb7ef33fd3e
Andrei <44436559+andreiionescu333@users.noreply.github.com>
2018-11-12 15:58:50 +0200

    lab2

---
+++ spring-boot-docker/src/main/java/com/lab/SpringBoot/Controller/Controller.java
@@ -0,0 +1,17 @@
+package com.lab.SpringBoot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ceva")
+public class Controller {
+
+	@GetMapping("/hello/{name}")
+	public String HelloWorld(@PathVariable("name") String name) {
+		return "Hello " + name;
+	}
+
+}
